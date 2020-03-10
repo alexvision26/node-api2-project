42548bb2af1aa0eeb599dde0f7219a11e4e5ee6d
Alex Miller <alexmillerprojects@gmail.com>
2020-03-10 14:11:34 -0700

    added post functionality with req validation check

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -13,4 +13,23 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+
+  if (title && contents) {
+    Posts.insert(req.body)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "Error adding the post" });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post"
+    });
+  }
+});
+
 module.exports = router;
